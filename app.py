cc8ad6bdd9e2b83a93b3e75de85e2d2eb2c8a0df
dailyherald <tbroderick@dailyherald.com>
2018-04-05 17:55:33 -0500

    added Stucka timestamp

--- app.py
+++ app.py
@@ -12,13 +12,35 @@ def get_csv():
 	csv_list = list(csv_obj)
 	return csv_list
 
+
+# trying mike stucka's date 
+def get_big_timestamp(date_object=None):
+    import datetime
+    if not date_object:
+        date_object = datetime.datetime.now()
+    stamp = ""
+    # comment out below if you don't want "Wednesday" or similar in your string
+    stamp += datetime.datetime.strftime(date_object, "%A, ")
+    if date_object.month == 9:
+        stamp += "Sept. " +  datetime.datetime.strftime(date_object, "%d, %Y").lstrip("0")
+    elif date_object.month < 3 or date_object.month > 7:
+        stamp += datetime.datetime.strftime(date_object, "%b. ") + datetime.datetime.strftime(date_object, "%d").lstrip("0")
+    else:
+        stamp += datetime.datetime.strftime(date_object, "%B ") + datetime.datetime.strftime(date_object, "%d").lstrip("0")
+    # uncomment out below if you want the year
+    # stamp += datetime.datetime.strftime(date_object, ", %Y")
+    stamp += ", at "
+    stamp += datetime.datetime.strftime(date_object, "%I:%M %p").lstrip("0").replace("AM", "a.m.").replace("PM", "p.m.")
+    return(stamp)
+
 # create pages here
 
 @app.route("/")
 def index():
     template = 'index.html'
+    timestamp=get_big_timestamp()
     object_list = get_csv()
-    return render_template(template, object_list=object_list)
+    return render_template(template, timestamp=timestamp, object_list=object_list)
 
 # necessary to establish the app
 if __name__ == '__main__':
